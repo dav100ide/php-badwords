15ba000595c217e258111e0bde6bca19c8a2ea68
dav100ide <oreglio.dev@gmail.com>
2022-12-14 10:05:51 +0100

    correzione lc

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <?php
    $frase = 'ciao a tutti mi chiamo cicciogamer89. mi piace Fortnite';
-   $censurato = $_GET['ciao'];
+   $censurato = $_GET['parola'];
    $frase_nuova = str_replace($censurato, '***', $frase);
+   
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>bad words</title>
-</head>
-<body>
-   <p>
-      <?php echo $frase ?>
-   </p>
-   <em>
-      lunghezza frase:
-      <?php echo strlen($frase) ?>
-   </em>
-   <h3>
-      <?php echo $frase_nuova ?>
-   </h3>
-</body>
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>bad words</title>
+   </head>
+   <body>
+      <p>
+         <?php echo $frase; ?>
+      </p>
+      <em>
+         lunghezza frase:
+         <?php echo strlen($frase); ?>
+      </em>
+      <h3>
+         <?php echo $frase_nuova; ?>
+      </h3>
+   </body>
 </html>
 
